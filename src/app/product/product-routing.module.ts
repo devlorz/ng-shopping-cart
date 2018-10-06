5d305efdf2c591e9890a5948ed6fcdd8c9ff7858
Nutti Saelor <leelorz6@gmail.com>
2018-10-06 20:04:52 +0700

    change routing and theme color

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './containers/product-list/product-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/product', pathMatch: 'full' },
-  { path: 'product', component: ProductListComponent }
+  { path: '', component: ProductListComponent }
 ];
 
 @NgModule({
