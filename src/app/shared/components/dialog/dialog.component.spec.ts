f93e8a199d6ec040c3f56bf43fccc0769d053752
Nutti Saelor <leelorz6@gmail.com>
2018-10-23 23:58:53 +0700

    add dialog test

--- src/app/shared/components/dialog/dialog.component.spec.ts
+++ src/app/shared/components/dialog/dialog.component.spec.ts
@@ -1,14 +1,55 @@
+import { MaterialModule } from './../../../material/material.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 import { DialogComponent } from './dialog.component';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
-xdescribe('DialogComponent', () => {
+const dialogData = {
+  message: 'test dialog',
+  isConfirm: true
+};
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: 'view-container-directive'
+})
+class ViewContainerDirective {
+  constructor(public viewContainerRef: ViewContainerRef) {}
+}
+
+@Component({
+  selector: 'app-view-container-component',
+  template: `<view-container-directive></view-container-directive>`
+})
+class ViewContainerComponent {
+  @ViewChild(ViewContainerDirective)
+  childWithViewContainer: ViewContainerDirective;
+
+  get childViewContainer() {
+    return this.childWithViewContainer.viewContainerRef;
+  }
+}
+
+describe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
+  let overlayContainerElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [DialogComponent]
+      imports: [MaterialModule],
+      declarations: [DialogComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: dialogData },
+        {
+          provide: OverlayContainer,
+          useFactory: () => {
+            overlayContainerElement = document.createElement('div');
+            return { getContainerElement: () => overlayContainerElement };
+          }
+        }
+      ]
     }).compileComponents();
   }));
 
